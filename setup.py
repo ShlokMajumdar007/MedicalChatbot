9e5e63d978f0d3f0f23af4789e9924c6e59624f2
Shlok Majumdar <shlokmajumdar04@gmail.com>
2026-02-04 12:54:48 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Shlok Majumdar',
+    author_email= 'shlokmaumdar04@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
